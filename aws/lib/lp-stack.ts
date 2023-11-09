5bfe48ac6aec2f14f44da1d51ab3a081c2417885
Louis-Philippe Turmel <louisphilippe.turmel@gmail.com>
2023-11-09 00:24:58 -0500

    Updated lp service executor to use a schedule (timezone support) to prevent DLS issues

--- aws/lib/lp-stack.ts
+++ aws/lib/lp-stack.ts
@@ -3,30 +3,33 @@ import { Construct } from 'constructs';
 import * as events from "aws-cdk-lib/aws-events";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as targets from "aws-cdk-lib/aws-events-targets";
+import { Schedule, ScheduleExpression } from "@aws-cdk/aws-scheduler-alpha";
+import { LambdaInvoke } from "@aws-cdk/aws-scheduler-targets-alpha";
 import { join } from 'path';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { TimeZone } from 'aws-cdk-lib';
 
 interface LeaguePointServiceProps extends cdk.StackProps {
     webhook_handler: lambda.Function;
 }
 export class LeaguePointService extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: LeaguePointServiceProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: LeaguePointServiceProps) {
+        super(scope, id, props);
 
-    const prefix = "monke-league-point-service";
-    const table = new Table(this, `${prefix}-table`, {
-        tableName: `${prefix}-table`,
-        partitionKey: {
-            name: "id",
-            type: AttributeType.STRING,
-        },
-        sortKey: {
-            name: "sk",
-            type: AttributeType.STRING,
-        },
-        billingMode: BillingMode.PAY_PER_REQUEST,
-    });
-            const lpHandler = new lambda.Function(
+        const prefix = "monke-league-point-service";
+        const table = new Table(this, `${prefix}-table`, {
+            tableName: `${prefix}-table`,
+            partitionKey: {
+                name: "id",
+                type: AttributeType.STRING,
+            },
+            sortKey: {
+                name: "sk",
+                type: AttributeType.STRING,
+            },
+            billingMode: BillingMode.PAY_PER_REQUEST,
+        });
+        const lpHandler = new lambda.Function(
             this,
             `${prefix}-api-handler`,
             {
@@ -53,24 +56,24 @@ export class LeaguePointService extends cdk.Stack {
         table.grantReadWriteData(lpHandler);
         table.grantReadWriteData(props?.webhook_handler!);
 
-        new events.Rule(
-            this,
-            `${prefix}-morning-schedule`,
-            {
-                schedule: events.Schedule.expression("cron(0 4 * * ? *)"),
-                targets: [new targets.LambdaFunction(lpHandler)],
-            },
-        );
-
-        new events.Rule(
-            this,
-            `${prefix}-nightly-schedule`,
-            {
-                schedule: events.Schedule.expression("cron(59 3 * * ? *)"),
-                targets: [new targets.LambdaFunction(lpHandler)],
-            }, 
-        );
-
-  }
+        new Schedule(this, `${prefix}-new--schedule`, {
+            schedule: ScheduleExpression.cron({
+                minute: '0',
+                hour: '0',
+                timeZone: TimeZone.AMERICA_NEW_YORK,
+            }),
+            target: new LambdaInvoke(lpHandler, {}),
+            description: 'This is a schedule to pull the League Points of users every day at midnight',
+        });
+        new Schedule(this, `${prefix}-new-schedule`, {
+            schedule: ScheduleExpression.cron({
+                minute: '59',
+                hour: '23',
+                timeZone: TimeZone.AMERICA_NEW_YORK,
+            }),
+            target: new LambdaInvoke(lpHandler, {}),
+            description: 'This is a schedule to pull the League Points of users every day before midnight',
+        });
+    }
 }
 
